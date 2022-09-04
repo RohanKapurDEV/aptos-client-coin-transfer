94ed33b22493d19f0d2a3548d3be05db7822c360
RohanXKapur <rohankapurdev@outlook.com>
2022-09-05 00:27:50 +0800

    finish example

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use once_cell::sync::Lazy;
 use std::str::FromStr;
 use url::Url;
 
-// :!:>section_1c
+// Use APTOS_NODE_URL environment variable to set the node URL or default to hardcoded value
 static NODE_URL: Lazy<Url> = Lazy::new(|| {
     Url::from_str(
         std::env::var("APTOS_NODE_URL")
@@ -17,6 +17,7 @@ static NODE_URL: Lazy<Url> = Lazy::new(|| {
     .unwrap()
 });
 
+// Use APTOS_FAUCET_URL environment variable to set the node URL or default to hardcoded value
 static FAUCET_URL: Lazy<Url> = Lazy::new(|| {
     Url::from_str(
         std::env::var("APTOS_FAUCET_URL")
@@ -26,12 +27,102 @@ static FAUCET_URL: Lazy<Url> = Lazy::new(|| {
     )
     .unwrap()
 });
-// <:!:section_1c
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Initialize clients
     let rest_client = Client::new(NODE_URL.clone());
     let faucet_client = FaucetClient::new(FAUCET_URL.clone(), NODE_URL.clone());
+    let coin_client = CoinClient::new(&rest_client);
+
+    // Initialize local accounts for alice and bob
+    // alice is marked as mutable since it needs to be for the coin_client.transfer call
+    let mut alice = LocalAccount::generate(&mut rand::rngs::OsRng);
+    let bob = LocalAccount::generate(&mut rand::rngs::OsRng);
+
+    println!("\n===== Local Accounts =====");
+    println!("Alice: {}", alice.address().to_hex_literal());
+    println!("Bob: {}", bob.address().to_hex_literal());
+
+    // Create and fund Alice's onchain account. Create Bob's onchain account
+    faucet_client
+        .fund(alice.address(), 20_000)
+        .await
+        .context("Failed to fund Alice")?;
+    faucet_client
+        .create_account(bob.address())
+        .await
+        .context("Failed to create onchain account for Bob")?;
+
+    println!("\n===== Initial balances =====");
+    println!(
+        "Alice: {:?}",
+        coin_client
+            .get_account_balance(&alice.address())
+            .await
+            .context("Could not fetch Alice's balance")?
+    );
+    println!(
+        "Bob: {:?}",
+        coin_client
+            .get_account_balance(&bob.address())
+            .await
+            .context("Could not fetch Bob's balance")?
+    );
+
+    // Transfer 1000 coins from Alice to Bob
+    let tx_hash = coin_client
+        .transfer(&mut alice, bob.address(), 1000, None)
+        .await
+        .context("Failed to transfer coins from Alice to Bob")?;
+
+    rest_client
+        .wait_for_transaction(&tx_hash)
+        .await
+        .context("Failed to wait for transaction")?;
+
+    println!("\n===== Intermediate balances =====");
+    println!(
+        "Alice: {:?}",
+        coin_client
+            .get_account_balance(&alice.address())
+            .await
+            .context("Could not fetch Alice's balance")?
+    );
+    println!(
+        "Bob: {:?}",
+        coin_client
+            .get_account_balance(&bob.address())
+            .await
+            .context("Could not fetch Bob's balance")?
+    );
+
+    // Transfer 1000 coins from Alice to Bob
+    let tx_hash = coin_client
+        .transfer(&mut alice, bob.address(), 1000, None)
+        .await
+        .context("Failed to transfer coins from Alice to Bob")?;
+
+    rest_client
+        .wait_for_transaction(&tx_hash)
+        .await
+        .context("Failed to wait for transaction")?;
+
+    println!("\n===== Final balances =====");
+    println!(
+        "Alice: {:?}",
+        coin_client
+            .get_account_balance(&alice.address())
+            .await
+            .context("Could not fetch Alice's balance")?
+    );
+    println!(
+        "Bob: {:?}",
+        coin_client
+            .get_account_balance(&bob.address())
+            .await
+            .context("Could not fetch Bob's balance")?
+    );
 
     Ok(())
 }
